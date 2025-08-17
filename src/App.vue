48ddf1c9f5b0c1bf039cefb9bf5a4f52c83dc670
ko5ba <potapovdaniil33@gmail.com>
2025-08-17 16:09:59 +0300

    feat(button): add custom color properties and integrate Roboto font
    - Add custom CSS properties for button colors:
      -
      -
      -
    - Integrate 'Roboto' font family via @font-face
    - Update button styles to use custom properties and Roboto font
    - Ensure backward compatibility with default values

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
+import Button from './components/ui/Button.vue';
 const dateNow = new Date().toLocaleDateString('ru-RU')
 </script>
 
 <template>
   <p>Текущая дата: {{ dateNow }}</p>
+  <Button />
 </template>
 
 <style scoped>
---
+++ src/components/ui/Button.vue
@@ -0,0 +1,28 @@
+<script setup></script>
+
+<template>
+  <button class="btn">
+    Начать игру
+  </button>
+</template>
+
+<style scoped>
+.btn {
+  width: 335px;
+  height: 68px;
+  border-radius: 100px; 
+  padding: 16px 10px;
+  background-color: var(--color-primary-button);
+  color: white;
+  font-size: 24px;
+  line-height: 36px;
+  font-weight: 400; 
+  border: none;
+  cursor: pointer;
+  transition: background-color 0.2s;
+}
+
+.btn:hover {
+  background-color: var(--color-primary-button-hover);
+}
+</style>
